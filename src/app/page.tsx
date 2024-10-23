27288210d8754fdcc74ebbf543c742d56feb45dc
David Arzipe <daviaaljaque@gmail.com>
2024-10-23 07:21:40 -0600

    Create the nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="App">
-      <h1 className="title">Quizap</h1>
+      <nav className="nav">
+        <header className="flex gap-5">
+          <Image src="/favicon.ico" alt="Logo" width={75} height={75}></Image>
+          <h1 className="title">Quizap</h1>
+        </header>
+        <p className="description m-3 text-pretty max-w-xl">
+          Learn fun, fast and easy with quizap! Discoer all we free quizes that
+          you can view your knowledges and progress.
+        </p>
+      </nav>
     </div>
   );
 }
