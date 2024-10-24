483185a9d214fed6d90d1264f9cdfc3e9fa6bf01
David Arzipe <daviaaljaque@gmail.com>
2024-10-24 17:24:19 -0600

    style logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,13 @@ export default function Home() {
     <div className="App">
       <nav className="nav">
         <header className="flex gap-5">
-          <Image src="/favicon.ico" alt="Logo" width={75} height={75}></Image>
+          <Image
+            className="rounded-2xl w-24 h-24"
+            src="/favicon.ico"
+            alt="Logo"
+            width={75}
+            height={75}
+          ></Image>
           <h1 className="title">Quizap</h1>
         </header>
         <p className="description m-3 text-pretty max-w-xl">
