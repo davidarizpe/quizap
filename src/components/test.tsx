bd67b992d7711c8b75926e6ecf8e807e94c8f0f9
David Arzipe <daviaaljaque@gmail.com>
2024-11-02 18:01:41 -0600

    component test main modal

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,10 +1,44 @@
 "use client";
 import type { Test } from "@/types/Questions";
+import type { Selected } from "@/types/selected";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Test({ questions }: Test[]) {
+  const [selectedAnswers, setSelectedAnswers] = useState<Selected[]>([]);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [note, setNote] = useState<string | number>(0);
+
+  const route = useRouter();
+
+  const handleAnswerChange = (question: number, answer: number) => {
+    setSelectedAnswers((prev) => {
+      const updatedAnswers = prev.filter((item) => item.question !== question);
+      return [...updatedAnswers, { question, answer }];
+    });
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    alert("Submitted");
+    console.log(selectedAnswers);
+
+    let note = 0;
+
+    questions.forEach((question: Test, index: number) => {
+      const selectedAnswer = selectedAnswers.find(
+        (item) => item.question === index
+      );
+
+      if (selectedAnswer && selectedAnswer.answer === question.correct) {
+        note += 1;
+      }
+    });
+
+    const factor = 10 / questions.length;
+    const realnote = (note * factor).toFixed(1);
+
+    setNote(realnote);
+    setShowModal(true);
   };
 
   return (
@@ -13,13 +47,14 @@ export default function Test({ questions }: Test[]) {
         <div key={index} className="m-3">
           <h3 className="font-bold">{question.question}</h3>
           <ol type="a">
-            {question.answers.map((answer, index) => (
-              <li key={index} style={{ listStyleType: "lower-alpha" }}>
+            {question.answers.map((answer, itemIndex: number) => (
+              <li key={itemIndex} style={{ listStyleType: "lower-alpha" }}>
                 <input
                   type="radio"
                   className="m-2"
-                  name={question.question}
+                  name={`question-${index}`}
                   value={answer}
+                  onChange={() => handleAnswerChange(index, itemIndex)}
                 />
                 <label>{answer}</label>
               </li>
@@ -30,6 +65,26 @@ export default function Test({ questions }: Test[]) {
       <button type="submit" className="btn">
         Submit
       </button>
+
+      {showModal && (
+        <div className="modal fixed inset-0 w-full h-full bg-[rgba(0,0,0,0.5)] text-black">
+          <div
+            className="modal-content absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white py-[5rem] px-8
+          rounded-2xl shadow-lg flex justify-center items-center flex-col"
+          >
+            <button
+              className="absolute top-0 right-0 m-5 text-3xl cursor-pointer rounded-full transition hover:bg-gray-300 px-2 py-1"
+              onClick={() => {
+                setShowModal(false);
+                route.push("/");
+              }}
+            >
+              x
+            </button>
+            <h2 className="font-bold text-2xl md:text-3xl m-3">{note}/10</h2>
+          </div>
+        </div>
+      )}
     </form>
   );
 }
---
+++ src/types/selected.ts
@@ -0,0 +1,4 @@
+export interface Selected {
+  question: number;
+  answer: number;
+}
